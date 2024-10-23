1e9d122be7867c41de1f4890213e38ca20bd4513
Mario Lasić <mario.lasic@outlook.com>
2024-10-23 19:23:54 +0200

    Added streamlit html

--- main.py
+++ main.py
@@ -9,5 +9,12 @@ today = date.today()
 yesterday = today - timedelta(days = 1)
 url = f"https://api.nasa.gov/planetary/apod?api_key={api_key}&date={yesterday}"
 
-response = requests.get(url=url).text
-print(response)
+response = requests.get(url=url).json()
+
+st.html(
+    "<div style='text-align: center; margin: auto; width:60%'>"
+        f"<h1 style='margin-bottom: 1rem'>{response["title"]}</h1>"
+        f"<img src='{response["url"]}' style='object-fit: contain; width: 100%;'></img>"
+        f"<p style='margin-top: 20px; text-align: center;'>{response["explanation"]}</p>"
+    "</div>"
+)
